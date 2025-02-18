68757081d7c3621bdb7247afa7e30a603c300df2
Tamara Salinas <dev.tamara29@gmail.com>
2025-02-18 16:23:14 -0300

    seccion 16-terminada

--- src/modules/admin/views/ProductView.ts
+++ src/modules/admin/views/ProductView.ts
@@ -1,20 +1,27 @@
+import CustomInput from '@/modules/common/components/CustomInput.vue';
+import CustomTextArea from '@/modules/common/components/CustomTextArea.vue';
 import { getProductById } from '@/modules/products/actions';
 import { useQuery } from '@tanstack/vue-query';
-import { useForm } from 'vee-validate';
-import { defineComponent, watchEffect } from 'vue';
+import { useFieldArray, useForm } from 'vee-validate';
+
+import { defineComponent, watch, watchEffect } from 'vue';
 import { useRouter } from 'vue-router';
 import * as yup from 'yup';
 
 const validationSchema = yup.object({
-  tittle: yup.string().required(),
+  title: yup.string().required().min(2, 'Minimo de 2 letras'),
   slug: yup.string().required(),
   description: yup.string().required(),
   price: yup.number().required(),
   stock: yup.number().min(1).required(),
-  geder: yup.string().required().oneOf(['men', 'women', 'kid']),
+  gender: yup.string().required().oneOf(['men', 'women', 'kid']),
 });
 
 export default defineComponent({
+  components: {
+    CustomInput,
+    CustomTextArea,
+  },
   props: {
     productId: {
       type: String,
@@ -36,31 +43,65 @@ export default defineComponent({
       retry: false, //si falla la cantidad de reintentos
     });
 
-    const { values, defineField, errors } = useForm({
+    const { values, errors, defineField, handleSubmit, resetForm, meta } = useForm({
       validationSchema,
+      //  initialValues: product.value,
     });
 
-    const [tittle, tittleAttrs] = defineField('tittle');
+    const [title, titleAttrs] = defineField('title');
     const [slug, slugAttrs] = defineField('slug');
     const [description, descriptionAttrs] = defineField('description');
     const [price, priceAttrs] = defineField('price');
     const [stock, stockAttrs] = defineField('stock');
     const [gender, genderAttrs] = defineField('gender');
 
+    const { fields: sizes, remove: removeSize, push: pushSize } = useFieldArray<string>('sizes');
+    const { fields: images } = useFieldArray<string>('images'); // un arreglo de string
+
+    const onSubmit = handleSubmit((value) => {
+      console.log({ value });
+    });
+
+    const toggleSize = (size: string) => {
+      const currentSizes = sizes.value.map((s) => s.value);
+      const hasSize = currentSizes.includes(size);
+
+      if (hasSize) {
+        removeSize(currentSizes.indexOf(size));
+      } else {
+        pushSize(size);
+      }
+    };
+
     watchEffect(() => {
       if (isError.value && !isLoading.value) {
         //si hay un error y no estamos cargando
         router.replace('/admin/products'); //sacamos a la persona y la mandamos a la pantalla products
+        return;
       }
     });
-    console.log(product);
+    watch(
+      product,
+      () => {
+        if (!product) return;
+        resetForm({
+          values: product.value,
+        });
+      },
+      {
+        deep: true, //que este pendiente de las propiedades internas del objeto, pendiente de los objetos anidados
+        immediate: true, //que la cargue apenas se monte el componente
+      },
+    );
 
     return {
       //Properties
       values,
       errors,
-      tittle,
-      tittleAttrs,
+      meta,
+
+      title,
+      titleAttrs,
       slug,
       slugAttrs,
       description,
@@ -71,10 +112,20 @@ export default defineComponent({
       stockAttrs,
       gender,
       genderAttrs,
+      images,
+      sizes,
+
       //getters
       allSizes: ['XS', 'S', 'M', 'L', 'XL', 'XXL'],
 
       //actions
+      onSubmit,
+      toggleSize,
+
+      hasSize: (size: string) => {
+        const currentSizes = sizes.value.map((s) => s.value);
+        return currentSizes.includes(size);
+      },
     };
   },
 });
